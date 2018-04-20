915241db2d7d3066dd7651cfbaa8ac03622bb576
Chris Alberti <christopherjalberti@gmail.com>
2018-04-20 04:34:27 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_second_tier
 	monopoly = {}
 	monopoly[:railroads] = {}
 	monopoly[:pieces] = 4
-	monoploy[:railroads][:names] = {}
+	monopoly[:railroads][:names] = {}
 	monopoly
 end
 
